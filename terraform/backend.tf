0d4ce23e00bb8932a95b1efb303a06c10541f47b
Boldin Daniil <boldin@isd-ops.ru>
2026-06-09 13:14:29 +0300

    another fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,16 @@
 terraform {
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "fleetlord-backet"
-    region     = "ru-central1"
-    key        = "terraform.tfstate"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    bucket = "fleetlord_bucket"
+    region = "ru-central1"
+    key    = "terraform.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     force_path_style            = true
   }
