635c1d59c60904be7527f9c8f0378dc0b27ac654
Boldin Daniil <boldin@isd-ops.ru>
2026-06-09 13:12:04 +0300

    fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,10 +5,9 @@ terraform {
     region     = "ru-central1"
     key        = "terraform.tfstate"
 
-    access_key = var.access_key
-    secret_key = var.secret_key
-
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
   }
 }
